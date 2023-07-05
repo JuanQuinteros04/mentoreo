5add4b79fd90d501a169ce97a36bd6373dd270ce
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-07-05 17:05:48 -0300

    Adding repositories

---
+++ microservicio-alumnos/src/main/java/com/mentoresalumnos/persistence/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.mentoresalumnos.persistence;
+
+import com.mentoresalumnos.model.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
+}
---
+++ microservicio-alumnos/src/main/java/com/mentoresalumnos/persistence/MentorRepository.java
@@ -0,0 +1,9 @@
+package com.mentoresalumnos.persistence;
+
+import com.mentoresalumnos.model.Mentor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
+}
