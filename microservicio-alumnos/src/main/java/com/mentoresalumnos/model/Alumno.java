5afe5119583345599447f441d28337222a2edee8
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-07-05 16:59:54 -0300

    Adding Mentor entity

--- microservicio-alumnos/src/main/java/com/mentoresalumnos/model/Alumno.java
+++ microservicio-alumnos/src/main/java/com/mentoresalumnos/model/Alumno.java
@@ -1,9 +1,6 @@
 package com.mentoresalumnos.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -19,17 +16,23 @@ public class Alumno {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "nombre", nullable = false, unique = true)
     private String nombre;
 
+    @Column(name = "apellido")
     private String apellido;
 
+    @Column(name = "edad")
     private Long edad;
 
+    @Column(name = "nivel")
     private AlumnoNivel alumnoNivel;
 
+    @Column(name = "cantidad_mentores")
     private Long cantidadMentores;
 
-    @OneToMany
+    @Column(name = "mentores")
+    @OneToMany(fetch = FetchType.LAZY)
     private List<Mentor> mentores;
 
 
--- microservicio-alumnos/src/main/java/com/mentoresalumnos/model/Mentor.java
+++ microservicio-alumnos/src/main/java/com/mentoresalumnos/model/Mentor.java
@@ -1,14 +1,34 @@
 package com.mentoresalumnos.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Mentor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column()
+    private String nombre;
+
+    @Column()
+    private String apellido;
+
+    @Column()
+    private Long edad;
+
+    @Column()
+    @ManyToOne
+    private Alumno alumno;
+
 }
