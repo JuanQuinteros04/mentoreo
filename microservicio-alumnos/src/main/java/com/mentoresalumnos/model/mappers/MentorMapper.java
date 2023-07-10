e8192426d42028141cdb4fb1311f691fd0bf9e69
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-07-09 21:56:21 -0300

    Adding mappers

--- microservicio-alumnos/src/main/java/com/mentoresalumnos/model/mappers/MentorMapper.java
+++ microservicio-alumnos/src/main/java/com/mentoresalumnos/model/mappers/MentorMapper.java
@@ -1,4 +1,20 @@
 package com.mentoresalumnos.model.mappers;
 
-public class MentorMapper {
+import com.mentoresalumnos.model.Alumno;
+import com.mentoresalumnos.model.Mentor;
+import com.mentoresalumnos.model.dtos.AlumnoDTO;
+import com.mentoresalumnos.model.dtos.MentorDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MentorMapper {
+    MentorMapper INSTANCE = Mappers.getMapper(MentorMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Mentor mentorDTOToMentor(MentorDTO mentorDTO);
+
+
+    MentorDTO mentorToMentorDTO(Mentor mentor);
 }
