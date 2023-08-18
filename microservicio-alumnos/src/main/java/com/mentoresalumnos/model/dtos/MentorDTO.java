0927871ea3c4a106b9713fb108e3c5e5ae2b993c
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-08-18 16:10:49 -0300

    Adding @Builder annotation

--- microservicio-alumnos/src/main/java/com/mentoresalumnos/model/dtos/MentorDTO.java
+++ microservicio-alumnos/src/main/java/com/mentoresalumnos/model/dtos/MentorDTO.java
@@ -1,14 +1,12 @@
 package com.mentoresalumnos.model.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MentorDTO {
 
     private String name;
